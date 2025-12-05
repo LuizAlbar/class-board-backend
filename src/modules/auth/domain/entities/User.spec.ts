5c551859423ca723e1f23756ce20ea45ace65d0a
LuizAlbar <luizgustavo.ab@hotmail.com>
2025-12-05 15:11:42 -0300

    feat: add in memory users repository

--- src/modules/auth/domain/entities/User.spec.ts
+++ src/modules/auth/domain/entities/User.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { User, UserRole } from "./User.ts";
 
 describe("User Entity", () => {
---
+++ src/modules/auth/domain/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,61 @@
+import { randomUUID } from "node:crypto";
+import { User, UserRole } from "../../entities/User.ts";
+import type { UsersRepository } from "../users-repository.ts";
+
+export class InMemoryUsersRepository implements UsersRepository {
+	public items: User[] = [];
+
+	async findById(id: string) {
+		const user = this.items.find((item) => item.id === id);
+
+		if (!user) {
+			return null;
+		}
+
+		return user;
+	}
+
+	async findByEmail(email: string) {
+		const user = this.items.find((item) => item.email === email);
+
+		if (!user) {
+			return null;
+		}
+
+		return user;
+	}
+	async create(data: User) {
+		const user = new User({
+			id: randomUUID(),
+			name: data.name,
+			email: data.email,
+			password: data.password,
+			created_at: new Date(),
+			updated_at: new Date(),
+			role: UserRole.ESTUDANTE,
+		});
+
+		this.items.push(user);
+
+		return user;
+	}
+	async update(data: User): Promise<User> {
+		const index = this.items.findIndex((u) => u.id === data.id);
+
+		if (index === -1) {
+			throw Error;
+		}
+		this.items[index] = data;
+
+		return data;
+	}
+	async delete(id: string): Promise<void> {
+		const index = this.items.findIndex((u) => u.id === id);
+
+		if (index === -1) {
+			throw Error;
+		}
+
+		this.items.splice(index, 1);
+	}
+}
