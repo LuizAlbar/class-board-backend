a5b50dbe8b6bc8a542fe94921f211dadb092d601
LuizAlbar <luizgustavo.ab@hotmail.com>
2025-12-14 14:16:15 -0300

    feat: add create user dto

---
+++ src/modules/auth/application/dtos/user-dtos.ts
@@ -0,0 +1,17 @@
+import type { UserRole } from "../../domain/entities/User.ts";
+
+export interface UserDto {
+    id: string;
+	name: string;
+	email: string;
+	created_at: Date;
+	updated_at: Date;
+	role: UserRole;
+}
+
+export interface CreateUserDTO {
+	name: string;
+	email: string;
+	password: string;
+	role: UserRole;
+}
