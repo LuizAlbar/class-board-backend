1d92f7ecafa46478a0f6f1520d37545ec9395ddf
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-01-19 20:50:57 -0300

    test: add create organization session tests

---
+++ src/modules/auth/application/use-cases/create-organization-session.spec.ts
@@ -0,0 +1,55 @@
+import { randomUUID } from "node:crypto";
+import { beforeEach, describe, expect, it } from "vitest";
+import {
+	Membership,
+	Role,
+} from "@/modules/membership/domain/entities/Membership.ts";
+import { InMemoryMembershipsRepository } from "@/modules/membership/domain/repositories/in-memory/in-memory-memberships-repository.ts";
+import { ForbiddenOrganizationError } from "../../domain/errors/forbidden-organization-error.ts";
+import { CreateOrganizationSessionUseCase } from "./create-organization-session.ts";
+import { AccessTokenServiceMock } from "./mocks/AccessTokenServiceMock.ts";
+
+let membershipsRepository: InMemoryMembershipsRepository;
+let sut: CreateOrganizationSessionUseCase;
+const accessTokenService = new AccessTokenServiceMock();
+
+const membershipData = new Membership({
+	id: randomUUID(),
+	userId: "user-id",
+	organizationId: "org-id",
+	role: Role.ESTUDANTE,
+	created_at: new Date(),
+	updated_at: new Date(),
+});
+
+describe("Create Organizaztion Session Use Case", () => {
+	beforeEach(() => {
+		membershipsRepository = new InMemoryMembershipsRepository();
+		sut = new CreateOrganizationSessionUseCase(
+			membershipsRepository,
+			accessTokenService,
+		);
+	});
+
+	it("should be able to create an organization session", async () => {
+		await membershipsRepository.create(membershipData);
+
+		const membership = await sut.execute({
+			userId: membershipData.userId,
+			organizationId: membershipData.organizationId,
+		});
+
+		expect(membership).toEqual(expect.any(String));
+	});
+
+	it("should not be possible to access a prohibited organization", async () => {
+		await membershipsRepository.create(membershipData);
+
+		await expect(() =>
+			sut.execute({
+				userId: membershipData.userId,
+				organizationId: "fake-org-id",
+			}),
+		).rejects.toBeInstanceOf(ForbiddenOrganizationError);
+	});
+});
