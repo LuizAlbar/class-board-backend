779869e2e11521ba89ceaa86cb217cdcfb365bdc
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-01-19 20:58:52 -0300

    feat: add create organization session factory

---
+++ src/modules/auth/infrastructure/factories/make-create-organization-session.ts
@@ -0,0 +1,15 @@
+import type { FastifyInstance } from "fastify";
+import { PrismaMembershipsRepository } from "@/modules/membership/infrastructure/database/repositories/prisma/prisma-memberships-repository.ts";
+import { CreateOrganizationSessionUseCase } from "../../application/use-cases/create-organization-session.ts";
+import { FastifyJWTProvider } from "../config/fastify-jwt.ts";
+
+export function makeCreateOrganizationSession(appInstance: FastifyInstance) {
+	const membershipRepository = new PrismaMembershipsRepository();
+	const accessTokenService = new FastifyJWTProvider(appInstance);
+	const createOrganizationSessionUseCase = new CreateOrganizationSessionUseCase(
+		membershipRepository,
+		accessTokenService,
+	);
+
+	return createOrganizationSessionUseCase;
+}
