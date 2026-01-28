8adfba8fcbde27c607b1b60dfbaf0bc32009a10f
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-01-28 20:40:01 -0300

    chore: set vite tests blocks -> unit & e2e

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
+import path from "node:path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: { environment: "node" },
+	plugins: [tsconfigPaths({ root: "./" })],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		projects: [
+			{
+				plugins: [tsconfigPaths({ root: "./" })],
+				resolve: {
+					alias: {
+						"@": path.resolve(__dirname, "./src"),
+					},
+				},
+				test: {
+					name: "unit",
+					include: ["src/modules/**/*.spec.ts"],
+					environment: "node",
+				},
+			},
+			{
+				plugins: [tsconfigPaths({ root: "./" })],
+				resolve: {
+					alias: {
+						"@": path.resolve(__dirname, "./src"),
+					},
+				},
+				test: {
+					name: "e2e",
+					include: ["src/modules/**/adapters/controllers/**/*.test.ts"],
+					environment: "node",
+				},
+			},
+		],
+	},
 });
