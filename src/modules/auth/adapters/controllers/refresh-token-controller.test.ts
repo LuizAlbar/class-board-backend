54d5a37c362686a15bcce8871df7fd7bb67b139f
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-01-29 11:37:13 -0300

    test: add it should be able to refresh token

---
+++ src/modules/auth/adapters/controllers/refresh-token-controller.test.ts
@@ -0,0 +1,27 @@
+import type { FastifyInstance } from "fastify";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { buildApp } from "@/app.ts";
+import { createAndAuthenticateUser } from "@/shared/utils/test/create-and-authenticate-user.ts";
+
+let app: FastifyInstance;
+describe("Refresh Token (e2e)", () => {
+	beforeAll(async () => {
+		app = await buildApp();
+		await app.ready();
+	});
+	afterAll(async () => {
+		app.close();
+	});
+
+	it("should be able to refresh token", async () => {
+		const { authResponse } = await createAndAuthenticateUser(app);
+		const cookies = authResponse.get("Set-Cookie") || [];
+
+		const refreshTokenResponse = await request(app.server)
+			.get("/auth/refresh-token")
+			.set("Cookie", cookies);
+
+		expect(refreshTokenResponse.statusCode).toEqual(200);
+	});
+});
