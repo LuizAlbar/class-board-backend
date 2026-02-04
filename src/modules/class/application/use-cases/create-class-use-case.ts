ca686530ddf99aef58a86971f53eb362e9bd1fa5
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-04 09:55:37 -0300

    feat: add create class use case

---
+++ src/modules/class/application/use-cases/create-class-use-case.ts
@@ -0,0 +1,34 @@
+import { randomUUID } from "node:crypto";
+import { type IUserContext, UserContextMapper } from "@/shared/auth/context.ts";
+import { getUserPermissions } from "@/shared/auth/get-user-permissions.ts";
+import { ForbiddenActionError } from "@/shared/errors/http-errors.ts";
+import { Class } from "../../domain/entities/class-entity.ts";
+import type { IClassesRepository } from "../../domain/repositories/class-repository.ts";
+import type { ICreateClassDTO } from "../dtos/class-dto.ts";
+
+export class CreateClassUseCase {
+	constructor(private membershipRepository: IClassesRepository) {}
+
+	async execute(dto: ICreateClassDTO, userContext: IUserContext) {
+		const newClass = new Class({
+			id: randomUUID(),
+			name: dto.name,
+			period: dto.period,
+			year: dto.year,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		});
+
+		const context = UserContextMapper.toModel(userContext);
+		const { cannot } = getUserPermissions(context.userId, context.role);
+		if (cannot("create", "Membership")) {
+			throw new ForbiddenActionError(
+				"You don't have permission to create classes",
+			);
+		}
+
+		const classItem = await this.membershipRepository.create(newClass);
+
+		return { classItem };
+	}
+}
