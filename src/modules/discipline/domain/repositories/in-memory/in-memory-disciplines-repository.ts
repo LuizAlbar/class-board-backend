fc85317b7c6c21bb7c20c519a9dc0135597ec4d6
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-05 11:03:48 -0300

    feat: add in memory disciplines repository

---
+++ src/modules/discipline/domain/repositories/in-memory/in-memory-disciplines-repository.ts
@@ -0,0 +1,49 @@
+import { randomUUID } from "node:crypto";
+import type {
+	ICreateDisciplineDTO,
+	IUpdateDisciplineDTO,
+} from "@/modules/discipline/application/dtos/discipline-dto.ts";
+import { Discipline } from "../../entities/discipline-entity.ts";
+import type { IDisciplinesRepository } from "../disciplines-repository.ts";
+
+export class InMemoryDisciplinesRepository implements IDisciplinesRepository {
+	public items: Discipline[] = [];
+	async findById(id: string) {
+		const discipline = this.items.find((item) => item.id === id);
+
+		if (!discipline) return null;
+
+		return discipline;
+	}
+	async findByName(query: string) {
+		const discipline = this.items.filter((item) => item.name === query);
+
+		if (!discipline) return null;
+		return discipline;
+	}
+	async create(data: ICreateDisciplineDTO) {
+		const newDiscipline = new Discipline({
+			id: randomUUID(),
+			name: data.name,
+			description: data.description,
+		});
+
+		this.items.push(newDiscipline);
+		return newDiscipline;
+	}
+	async update(id: string, data: IUpdateDisciplineDTO) {
+		const disciplineIndex = this.items.findIndex((item) => item.id === id);
+		const updatedDiscipline = new Discipline({
+			...this.items[disciplineIndex].props,
+			...data,
+		});
+
+		this.items[disciplineIndex] = updatedDiscipline;
+
+		return updatedDiscipline;
+	}
+	async delete(id: string) {
+		const disciplineIndex = this.items.findIndex((item) => item.id === id);
+		this.items.splice(disciplineIndex, 1);
+	}
+}
